1e011544ba4e476f8b5aa42817f1e9f489d0e5bc
hushuning <36027102@qq.com>
2025-05-19 10:08:41 +0800

    ss

--- gameS/depoly.js
+++ gameS/depoly.js
@@ -16,9 +16,26 @@ async function main() {
   //   });
   // console.log("BombGame deployed to:", bombGame.target);
   // ccdr.game = bombGame.getAddress();
-  const Defi = await hre.ethers.getContractFactory("DefiGame");
+  // const Defi = await hre.ethers.getContractFactory("DefiGame");
 
-  const defi = await Defi.deploy("0x66a533161b391feb7d80a02e0eac461ee3b583ef",{
+  // const defi = await Defi.deploy("0x66a533161b391feb7d80a02e0eac461ee3b583ef",{
+  //     // 把小费至少设成 1 gwei（1000000000 wei）
+  //     maxPriorityFeePerGas: hre.ethers.parseUnits("1", "gwei"),
+  //     // 总费上限也要足够覆盖 baseFee + priorityFee
+  //     maxFeePerGas:      hre.ethers.parseUnits("30", "gwei"),
+  //     // 可选：显式设 gasLimit
+  //     gasLimit:          3_000_000,
+  //   });
+
+  
+
+ 
+  // console.log("BombGame deployed to:", ccdr.bombGame);
+  // ccdr.defi = defi.getAddress();
+  // console.log("BombGame deployed to:", ccdr);
+  const Lmit = await hre.ethers.getContractFactory("LimitOrderProtocol");
+
+  const lmit = await Lmit.deploy({
       // 把小费至少设成 1 gwei（1000000000 wei）
       maxPriorityFeePerGas: hre.ethers.parseUnits("1", "gwei"),
       // 总费上限也要足够覆盖 baseFee + priorityFee
@@ -30,9 +47,11 @@ async function main() {
   
 
  
-  // console.log("BombGame deployed to:", ccdr.bombGame);
-  ccdr.defi = defi.getAddress();
+  console.log("BombGame deployed to:", ccdr.lmit);
+  ccdr.lmit = lmit.getAddress();
   console.log("BombGame deployed to:", ccdr);
+  
+
 }
 
 main().catch((error) => {
